565a9eb55296c7110983da60bf223fe10ece6b31
chiyaqjh <chiyaqjh@gmail.com>
2025-11-21 19:18:11 +0800

    change stbc

--- comp5521stablecoin.sol
+++ comp5521stablecoin.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract COMP5521Dollar is ERC20 {
     address public owner;
+    
+    // 免授权合约白名单
+    mapping(address => bool) public approvedContracts;
     
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
@@ -15,23 +17,40 @@ contract COMP5521Dollar is ERC20 {
     constructor() ERC20("COMP5521 Dollar", "C5D") {
         owner = msg.sender;
         _mint(msg.sender, 1 * 10 ** decimals());
+        
     }
     
     function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount* 10 ** decimals());
+        _mint(to, amount * 10 ** decimals());
     }
 
-    uint256 public faucetAmount = 1000 * 10 ** decimals(); // 100个代币
+    uint256 public faucetAmount = 1000 * 10 ** decimals();
     mapping(address => uint256) public lastFaucetTime;
     uint256 public faucetCooldown = 1 seconds;
 
     function useFaucet() external {
-    require(block.timestamp >= lastFaucetTime[msg.sender] + faucetCooldown, "Faucet cooldown");
-    
-    lastFaucetTime[msg.sender] = block.timestamp;
-    _mint(msg.sender, faucetAmount);
+        require(block.timestamp >= lastFaucetTime[msg.sender] + faucetCooldown, "Faucet cooldown");
+        lastFaucetTime[msg.sender] = block.timestamp;
+        _mint(msg.sender, faucetAmount);
     }
 
+    function addApprovedContract(address contractAddress) external onlyOwner {
+        approvedContracts[contractAddress] = true;
+    }
+
+    function removeApprovedContract(address contractAddress) external onlyOwner {
+        approvedContracts[contractAddress] = false;
+    }
+    
+    function transferFromAutomated(
+        address from, 
+        address to, 
+        uint256 amount
+    ) external returns (bool) {
+        require(approvedContracts[msg.sender], "Not approved contract");
+        _transfer(from, to, amount);
+        return true;
+    }
 
     function getTokenInfo() public view returns (
         string memory tokenName,
